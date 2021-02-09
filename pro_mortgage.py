efc23fe446174bd2ce3aa751cd1881338da0d6b8
Vaskovics <78701608+Vaskovics@users.noreply.github.com>
2021-02-09 15:05:20 +0000

    Add files via upload

---
+++ pro_mortgage.py
@@ -0,0 +1,26 @@
+amount = int(input("Please enter the amout of mortgage: "))
+percentage = int(input("Please enter interest rate: "))
+period = int(input("Please enter period of mortgage: "))
+
+month = period * 12
+
+month_body = amount/month
+
+pm = month
+mouth_interest = 0
+balance = amount
+
+for i in range(month -1 ):
+    pm = pm - 1
+    balance -= month_body
+    interest = (balance * (percentage/100)) / pm
+    mouth_interest += round(interest, 2)
+    pm = pm - 1
+
+print("*"* 40)
+
+
+mouth_interest_payment = mouth_interest / 11
+suma1 = month_body + mouth_interest_payment
+suma = round(suma1, 2)
+print(f"You month payment is {suma}£")
